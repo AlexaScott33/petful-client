56dfcc809910ae988962c134878bbbafb46c12b2
Alexa Scott <arscott4792@gmail.com>
2018-05-19 14:40:15 -0700

    created dogsReducer

--- src/reducers/cat.js
+++ src/reducers/cat.js
@@ -2,10 +2,9 @@ import {
     FETCH_CAT_REQUEST,
     FETCH_CAT_SUCCESS,
     FETCH_CAT_ERROR,
-    ADOPT_CAT_ERROR,
+    ADOPT_CAT_REQUEST,
     ADOPT_CAT_SUCCESS,
-    ADOPT_CAT_ERROR,
-    ADOPT_CAT_REQUEST
+    ADOPT_CAT_ERROR
 } from '../actions/cat';
 
 const initialState = {
--- src/reducers/dog.js
+++ src/reducers/dog.js
@@ -1,26 +1,54 @@
-// import {
-//     FETCH_DOG_REQUEST,
-//     FETCH_DOG_ERROR,
-//     FETCH_DOG_SUCCESS,
-//     ADOPT_DOG_ERROR,
-//     ADOPT_DOG_REQUEST,
-//     ADOPT_DOG_SUCCESS
-// } from '../actions/dog';
+import {
+    FETCH_DOG_REQUEST,
+    FETCH_DOG_SUCCESS,
+    FETCH_DOG_ERROR,
+    ADOPT_DOG_REQUEST,
+    ADOPT_DOG_SUCCESS,
+    ADOPT_DOG_ERROR
+} from '../actions/dog';
 
 const initialState = {
-    dogs: [{
-        imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-        imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-        name: 'Zeus',
-        sex: 'Male',
-        age: 3,
-        breed: 'Golden Retriever',
-        story: 'Owner Passed away'
-      }],
+    dogs: [],
     loading: false,
     error: null
 }
 
 export default function dogsReducer(state=initialState, action) {
+    if (action.type === FETCH_DOG_REQUEST) {
+        return Object.assign({}, state, {
+            loading: true
+        });
+    }
+    else if (action.type === FETCH_DOG_SUCCESS) {
+        return Object.assign({}, state, {
+            dogs: action.dogs,
+            loading: false,
+            error: null
+        });
+    }
+    else if (action.type === FETCH_DOG_ERROR) {
+        return Object.assign({}, state, {
+            loading: false,
+            error: action.error
+        });
+    }
+    else if (action.type === ADOPT_DOG_REQUEST) {
+        return Object.assign({}, state, {
+            loading: true
+        });
+    }
+    else if (action.type === ADOPT_DOG_SUCCESS) {
+        return Object.assign({}, state, {
+            dogs: action.dogs,
+            loading: false,
+            error: null
+        });
+    }
+    else if (action.type === ADOPT_DOG_ERROR) {
+        return Object.assign({}, state, {
+            loading: false,
+            error: action.error
+        });
+    }
     return state;
 }
