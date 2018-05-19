fc5b826d33c1ed342b45645181d04f485faa3cd2
Alexa Scott <arscott4792@gmail.com>
2018-05-19 14:37:42 -0700

    created catsReducer

--- src/reducers/cat.js
+++ src/reducers/cat.js
@@ -1,26 +1,55 @@
-// import {
-//     FETCH_CAT_REQUEST,
-//     FETCH_CAT_SUCCESS,
-//     FETCH_CAT_ERROR,
-//     ADOPT_CAT_ERROR,
-//     ADOPT_CAT_SUCCESS,
-//     ADOPT_CAT_ERROR
-// } from '../actions/cat';
+import {
+    FETCH_CAT_REQUEST,
+    FETCH_CAT_SUCCESS,
+    FETCH_CAT_ERROR,
+    ADOPT_CAT_ERROR,
+    ADOPT_CAT_SUCCESS,
+    ADOPT_CAT_ERROR,
+    ADOPT_CAT_REQUEST
+} from '../actions/cat';
 
 const initialState = {
-    cats: [{
-        imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-        imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-        name: 'Fluffy',
-        sex: 'Female',
-        age: 2,
-        breed: 'Bengal',
-        story: 'Thrown on the street'
-      }],
+    cats: [],
     loading: false,
     error: null
 }
 
 export default function catsReducer(state=initialState, action) {
+    if (action.type === FETCH_CAT_REQUEST) {
+        return Object.assign({}, state, {
+            loading: true
+        });
+    }
+    else if (action.type === FETCH_CAT_SUCCESS) {
+        return Object.assign({}, state, {
+            cats: action.cats,
+            loading: false,
+            error: null
+        });
+    }
+    else if (action.type === FETCH_CAT_ERROR) {
+        return Object.assign({}, state, {
+            loading: false,
+            error: action.error
+        });
+    }
+    else if (action.type === ADOPT_CAT_REQUEST) {
+        return Object.assign({}, state, {
+            loading: true
+        });
+    }
+    else if (action.type === ADOPT_CAT_SUCCESS) {
+        return Object.assign({}, state, {
+            cats: action.cats,
+            loading: false,
+            error: null
+        });
+    }
+    else if (action.type === ADOPT_CAT_ERROR) {
+        return Object.assign({}, state, {
+            loading: false,
+            error: action.error
+        });
+    }
     return state;
 }
