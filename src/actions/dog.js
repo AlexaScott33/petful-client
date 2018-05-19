c47a3632846b8ca727739d50263d842a4dcdfa22
Alexa Scott <arscott4792@gmail.com>
2018-05-19 13:58:45 -0700

    created sync and async actions for adoptDog

--- src/actions/dog.js
+++ src/actions/dog.js
@@ -32,3 +32,45 @@ export const fetchDog = () => dispatch => {
         dispatch(fetchDogError(err));
     });
 };
+
+export const ADOPT_DOG_REQUEST = 'ADOPT_DOG_REQUEST';
+export const adoptDogRequest = () => ({
+    type: ADOPT_DOG_REQUEST
+});
+
+export const ADOPT_DOG_SUCCESS = 'ADOPT_DOG_SUCCESS';
+export const adoptDogSuccess = dogs => ({
+    type: ADOPT_DOG_SUCCESS,
+    dogs
+});
+
+export const ADOPT_DOG_ERROR = 'ADOPT_DOG_ERROR';
+export const adoptDogError = error => ({
+    type: ADOPT_DOG_ERROR,
+    error
+});
+
+export const adoptDog = () => dispatch => {
+    dispatch(adoptDogRequest());
+    console.log('adopting dog!');
+    fetch(`${API_BASE_URL}/api/dog`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => {
+        if (!res.ok) {
+            return Promise.reject(res.statusText);
+        }
+        return res.json();
+    })
+    .then(dogs => {
+        dispatch(adoptDogSuccess(dogs));
+    })
+    .then(() => {
+        dispatch(fetchDog());
+    }).catch(err => {
+        dispatch(adoptDogError(err));
+    });
+};
