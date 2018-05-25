034ea8f6d115a2413a9bec35b5994d4ed9d49a1b
Alexa Scott <arscott4792@gmail.com>
2018-05-24 17:46:34 -0700

    created readme and popup on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Popup from "reactjs-popup";
 import { connect } from 'react-redux';
 import Cat from './Cat';
 import Dog from './Dog';
@@ -17,7 +18,16 @@ export class Dashboard extends React.Component {
     render() {
         return (
             <div>
-                <h1>Petful</h1>
+                <div className="header-box">
+                <Popup trigger={<button className="popup-button"> What is Petful?</button>} position="bottom">
+                        <div className="popup-box">
+                            <p><strong>Petful</strong> allows you to adopt either a cat or dog.</p>
+                            <p> The adoption process is based on a first come, first serve. The animals available  to adopt are the ones that have been in the shelter the longest.</p>
+                            <p> View the details about each pet and adopt one today!</p>
+                        </div>
+                    </Popup>
+                    <h1>Petful</h1>
+                </div>
                 <Cat cat={this.props.cats} onAdoptPet={() => this.props.dispatch(adoptCat())}/>
                 <Dog dog={this.props.dogs} onAdoptPet={() => this.props.dispatch(adoptDog())}/>
             </div>
